f8b130be073259051f43e9ef0e8fdc0d8715e6e1
sdhex <studulstu@gmail.com>
2023-07-29 11:54:24 +0300

    hotfix: limiter config

--- middlewares/rate-limiter.js
+++ middlewares/rate-limiter.js
@@ -1,8 +1,8 @@
 const rateLimiter = require('express-rate-limit');
 
 const limiter = rateLimiter({
-  max: 5,
-  windowMS: 10000,
+  max: 130,
+  windowMS: 100000,
   message: 'Вы больше не можете делать больше запросов в данный момент, попробуйте позже',
 });
 
